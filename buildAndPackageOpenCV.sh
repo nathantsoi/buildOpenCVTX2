1136c6dc3175e97f4e4d1089238ee7c8a0e125de
Dan Winkler <dan@6river.com>
2019-02-27 16:33:09 -0500

    fix: Fix build scripts.

--- buildAndPackageOpenCV.sh
+++ buildAndPackageOpenCV.sh
@@ -242,17 +242,17 @@ export DEBIAN_PACKAGE_SCRIPTS="OpenCV-3.4.5-${OPENCV_ARCH}-scripts.deb"
 time curl \
 	-H "X-JFrog-Art-Api: ${ARTIFACTORY_PASSWORD}" \
 	-T "${OPENCV_SOURCE_DIR}/opencv/build/${DEBIAN_PACKAGE_DEV}" \
-	"https://sixriver.jfrog.io/sixriver/debian/pool/main/p/pcl/${DEBIAN_PACKAGE_DEV};deb.distribution=${DISTRO};deb.component=main;deb.architecture=${ARCH}"
+	"https://sixriver.jfrog.io/sixriver/debian/pool/main/o/opencv/${DEBIAN_PACKAGE_DEV};deb.distribution=${DISTRO};deb.component=main;deb.architecture=${ARCH}"
 
 time curl \
 	-H "X-JFrog-Art-Api: ${ARTIFACTORY_PASSWORD}" \
 	-T "${OPENCV_SOURCE_DIR}/opencv/build/${DEBIAN_PACKAGE_LIBS}" \
-	"https://sixriver.jfrog.io/sixriver/debian/pool/main/p/pcl/${DEBIAN_PACKAGE_LIBS};deb.distribution=${DISTRO};deb.component=main;deb.architecture=${ARCH}"
+	"https://sixriver.jfrog.io/sixriver/debian/pool/main/o/opencv/${DEBIAN_PACKAGE_LIBS};deb.distribution=${DISTRO};deb.component=main;deb.architecture=${ARCH}"
 
 time curl \
 	-H "X-JFrog-Art-Api: ${ARTIFACTORY_PASSWORD}" \
 	-T "${OPENCV_SOURCE_DIR}/opencv/build/${DEBIAN_PACKAGE_PYTHON}" \
-	"https://sixriver.jfrog.io/sixriver/debian/pool/main/p/pcl/${DEBIAN_PACKAGE_PYTHON};deb.distribution=${DISTRO};deb.component=main;deb.architecture=${ARCH}"
+	"https://sixriver.jfrog.io/sixriver/debian/pool/main/o/opencv/${DEBIAN_PACKAGE_PYTHON};deb.distribution=${DISTRO};deb.component=main;deb.architecture=${ARCH}"
 
 time curl \
 	-H "X-JFrog-Art-Api: ${ARTIFACTORY_PASSWORD}" \
